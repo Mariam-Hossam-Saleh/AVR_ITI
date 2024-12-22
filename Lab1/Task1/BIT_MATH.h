a8cd51c9cf06b417fd1dfe46b42fcca6b92adef1
Mariam Hossam <mariam.hossam.keshk@gmail.com>
2024-12-22 11:27:07 +0200

    Deleted Lab1 folder

--- Lab1/Task1/BIT_MATH.h
+++
@@ -1,9 +0,0 @@
-#ifndef BIT_MATH_H_
-#define BIT_MATH_H_
-
-#define SET_BIT(Reg,Bit) (Reg |= (1<<(Bit)))
-#define CLR_BIT(Reg,Bit) (Reg &= ~(1<<(Bit)))
-#define TOG_BIT(Reg,Bit) (Reg ^= (1<<(Bit)))
-#define GET_BIT(Reg,Bit) ((Reg >> Bit)&(0x01))
-
-#endif //BIT_MATH_H_
